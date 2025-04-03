219dc22df6bc85a99a0b76698f2c73d2a2baa61b
Scott1272 <Wellscreek@outlook.com>
2025-04-02 21:24:57 -0400

    Added_Test

---
+++ LanguageTracker/LanguageTracker.Tests/FileSaverTests.cs
@@ -0,0 +1,19 @@
+namespace LanguageTracker.Tests;
+using LanguageTracker;
+public class FileSaverTests
+{
+    FileSaver fileSaver;
+    string testFileName;
+    public FileSaverTests()   
+    { 
+        testFileName = "test.doc.txt";
+        fileSaver = new FileSaver(testFileName);
+    }    
+    [Fact]
+    public void Test_FileSaver_Append()
+    {
+        fileSaver.AppendLine("Hello World");
+        var contentFromFile = File.ReadAllText(testFileName);
+        Assert.Equal ("Hello World" + Environment.NewLine, contentFromFile);
+    }
+}
